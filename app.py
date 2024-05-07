0909e463fb5bf138994414d82292c7c6aedc2325
FilippoPietroNeri <neri.filippo@iisgalvanimi.edu.it>
2024-05-07 08:20:07 +0000

    css update

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def homepage():
     return render_template('index.html')
 
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3246, debug=True)
