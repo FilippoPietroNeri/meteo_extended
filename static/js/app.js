9d2e945b899a5bc3d666348383b975894d8ecd67
FilippoPietroNeri <neri.filippo@iisgalvanimi.edu.it>
2024-05-09 11:58:07 +0000

    added latest things and removed unused html

--- static/js/app.js
+++ static/js/app.js
@@ -1,19 +1,27 @@
 // Qui dobbiamo dichiarare tutti i testi,bottoni,divs per poi porteli modificare
 var input = document.querySelector('.input_text'); // document.querySelector(#nome oppure .nome) cioè trova tutti i tag con quella classe oppure quell'id
-var main = document.querySelector('#name');
 var temp = document.querySelector('.temp');
 var desc = document.querySelector('.desc');
 var clouds = document.querySelector('.clouds');
 var button = document.querySelector('.submit');
-var iconC = document.querySelector('#icon');
+var sunset = document.querySelector('.sunset');
+var sunrise = document.querySelector('.sunrise');
 var humidity = document.querySelector('.humidity');
-var wind = document.querySelector('.wind');
 var pressure = document.querySelector('.pressure');
+var wind = document.querySelector('.wind');
 
-var alertbox = document.querySelector('#alertbox');
+var iconC = document.querySelector('#icon');
+var main = document.querySelector('#name');
+var loading = document.querySelector('#loading');
+var moreinfo = document.querySelector('#moreinfo');
 var infobox = document.querySelector('#infobox');
+var alertbox = document.querySelector('#alertbox');
 var previsions = document.querySelector('#previsions');
 var boxprev = document.querySelector('#boxprev');
+
+var inputmeteo = document.getElementById('inputmeteo');
+var infobtn = document.getElementById('infobtn');
+var gotom = document.getElementById('gotom');
 
 var currentLon, currentLat; // dobbiamo salvare i dati per porteli vedere sulle altre funzioni
 
@@ -22,13 +30,11 @@ function changeVisibility(tag, bool) // metodo per cambiare velocemente la visib
   tag.style.display = bool ? "block" : "none" // Se bool è vero display diventa block (VISIBILE) oppure none (NON VISIBILE)
 }
 
-function gotom() {
-  document.location.href = `https://www.google.com/maps/@${currentLat},${currentLon},14z`
-}
-
-changeVisibility(infobox, false);
-button.addEventListener('click', function (name) { // Aggiungiamo un listener che ascolta per i click dell'utente
+function handleMeteo() {
   changeVisibility(alertbox, false);
+  changeVisibility(infobox, false);
+  changeVisibility(boxprev, false);
+
   // Facciamo una "fetch" richiesta ad un API (Application Programming Interface) per trovare tutti i dati sulla città che abbiamo messo.
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=2c6d58519b7fb9e4e84c000b499d9cec&units=metric`)
     .then(response => response.json()) // trasformiamo la risposta in un json
@@ -41,6 +47,9 @@ button.addEventListener('click', function (name) { // Aggiungiamo un listener ch
       var humidityValue = data['main']['humidity'];
       var windValue = data['wind']['speed'];
       var pressureValue = data['main']['pressure'];
+      var sunriseDate = data['sys']['sunrise'];
+      var sunsetDate = data['sys']['sunset'];
+
       currentLon = data['coord']['lon'];
       currentLat = data['coord']['lat'];
 
@@ -52,27 +61,36 @@ button.addEventListener('click', function (name) { // Aggiungiamo un listener ch
       humidity.innerHTML = `<strong>Humidity:</strong> ${humidityValue}%`;
       wind.innerHTML = `<strong>Wind Speed:</strong> ${windValue} m/s`;
       pressure.innerHTML = `<strong>Pressure:</strong> ${pressureValue} hPa`;
+      sunrise.innerHTML =`<strong>Alba:</strong> ${new Date(sunriseDate * 1000).toLocaleString('en-US')} time`;
+      sunset.innerHTML =`<strong>Tramonto:</strong> ${new Date(sunsetDate * 1000).toLocaleString('en-US')} time`;
       iconC.src = `http://openweathermap.org/img/w/${weatherIcon}.png`;
       input.value = "";
       changeVisibility(infobox, true);
+      changeVisibility(loading, true);
       fetch(`https://openweathermap.org/data/2.5/onecall?lat=${currentLat}&lon=${currentLon}&units=metric&appid=439d4b804bc8187953eb36d2a8c26a02`)
         .then(response => response.json()) // trasformiamo la risposta in un json
         .then(data => {
           var foredata = data['daily'];
           var listHTML = "";
-          for (var i = 0; i < 7; i++) {
-            var date_uni = new Date(foredata[i]['dt']*1000).toUTCString().slice(0,-17);
+          for (var i = 0; i < 7; i++) { // fa un loop della lista di Previsioni Giornaliere e ne ottiene 7 (una settimana).
+            // Prende le variabili dal json
+            var date_uni = new Date(foredata[i]['dt'] * 1000).toUTCString().slice(0, -17); // Arrotonda x 1000 il valore unix e converte a UTC + taglia il risultato per mostrare soltanto il Giorno, Mese e Numero
             var temp_min = Math.round(foredata[i]['temp']['min']);
             var temp_max = Math.round(foredata[i]['temp']['max']);
             var temp_ico = foredata[i]['weather'][0]['icon'];
             var temp_des = foredata[i]['weather'][0]['description'];
 
+            // Qua sotto aggiunge a listHTML ogni <li> con le informazioni.
             listHTML += `<li class="lilist"><span>${date_uni}</span><div class="list-values"><div style="display: flex; justify-content: flex-start; align-items: center;"><img src="http://openweathermap.org/img/w/${temp_ico}.png" alt="${temp_des}"><span>${temp_max} / ${temp_min} °C</span></div><span>${temp_des}</span></div></li>`
           }
+          // applica alle previsioni tutta la lista creata.
           previsions.innerHTML = listHTML;
+          // cambia la visibilità di tutto.
           changeVisibility(boxprev, true);
+          changeVisibility(loading, false);
         })
         .catch(err => {
+          // Eseguito quando c'è un errore evidente.
           changeVisibility(boxprev, false);
           changeVisibility(alertbox, true);
           alertbox.innerHTML = "<b>Warning!</b> Impossibile ottenere le previsioni di questa città";
@@ -80,11 +98,39 @@ button.addEventListener('click', function (name) { // Aggiungiamo un listener ch
         })
     })
     .catch(err => {
-      //alert("Nome della città inesistente")
       changeVisibility(alertbox, true);
       alertbox.innerHTML = "<b>Warning!</b> Nome della città inesistente"
       input.value = "";
       changeVisibility(infobox, false);
     }); // se la promise viene rifiutata manderà un alert 
+}
+
+
+// Aggiungiamo un listener che ascolta per i click dell'utente
+button.addEventListener('click', function (name) {
+  handleMeteo();
+});
+
+gotom.addEventListener('click', function (name) {
+  document.location.href = `https://www.google.com/maps/@${currentLat},${currentLon},14z` // Redirecta la pagina su google maps con le Longitudini e Latitudini date
 })
 
+infobtn.addEventListener('click', function (name) {
+  if (moreinfo.style.display == "block") { // Se il div delle informazioni è visibile allora lo rende invisibile
+    moreinfo.style.display = "none";
+    infobtn.textContent = "Show Info";
+  } else if (moreinfo.style.display == "none") {
+    moreinfo.style.display = "block";
+    infobtn.textContent = "Hide Info"
+  }
+})
+
+// Serve semplicemente per dire "Se l'utente clicca il pulsante ENTER quando è sull'input allora esegue il programma"
+inputmeteo.addEventListener('keyup', function (event) {
+  event.preventDefault();
+  if (event.key === 'Enter') {
+    handleMeteo();
+  }
+})
+
+changeVisibility(infobox, false);
